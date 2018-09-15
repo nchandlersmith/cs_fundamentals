3262ea28f358506c03ddb1b14586def10c9b6717
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2018-09-15 16:06:45 -0500

    Adding the hello <name> console application

---
+++ hello/hello/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+
+            message = "Hello, " + args[0] + "!";
+            Console.WriteLine(message);
+        }
+    }
+}
